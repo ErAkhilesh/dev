ff8d83305e26e69a843f4b98565c5ab27961d305
gakhi <gakhi@DESKTOP-DB9ED7V>
2023-01-19 10:25:55 +0530

    5th commit

---
+++ src/test/java/com/crm/gitHubpractic/gitHubSampeTest1.java
@@ -0,0 +1,13 @@
+package com.crm.gitHubpractic;
+
+import org.testng.annotations.Test;
+
+public class gitHubSampeTest1 {
+	@Test
+	public void create()
+	{
+		System.out.println("Created successfully");
+	}
+	
+}
+
---
+++ src/test/java/com/crm/gitHubpractic/gitHubSampleTest2.java
@@ -0,0 +1,13 @@
+package com.crm.gitHubpractic;
+
+import org.testng.annotations.Test;
+
+public class gitHubSampleTest2 {
+	@Test
+	public void delete()
+	{
+		System.out.println("deleted successfully");
+	}
+}
+
+
